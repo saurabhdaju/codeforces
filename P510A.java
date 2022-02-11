6168ce6a09b3ac4d0c954e9e8425711acf27434e
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-02-11 17:53:50 +0530

    Replaced with a better approach

--- P510A.java
+++ P510A.java
@@ -2,29 +2,18 @@ package com.codeforces;
 import java.util.Scanner;
 public class P510A {
     public static void main(String[] args) {
+        //Nested loop approach
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        String snake = "#";
-        String first = "";
-        String last = "#";
-        for(int i=1; i<m; i++) {
-            snake += "#";
-        }
-        for(int i=1; i<m; i++) {
-            first += ".";
-        }
-        String bin = "";
         for(int i=1; i<=n; i++) {
-            if(i%2 != 0)
-                System.out.println(snake);
-            else {
-                System.out.print(first);
-                System.out.println(last);
-                bin = first;
-                first = last;
-                last = bin;
+            for(int j=1; j<=m; j++) {
+                if ((i % 2 != 0) || (i % 4 == 2 && j == m) || (i % 4 == 0 && j == 1))
+                    System.out.print('#');
+                else
+                    System.out.print('.');
             }
+            System.out.println();
         }
     }
 }
