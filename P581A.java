379a720750fdbb01369514ef9ccbc5cfdaf5f56a
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-02-12 15:40:25 +0530

    50 :D

--- P581A.java
+++ P581A.java
@@ -4,17 +4,6 @@ public class P581A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        if(a>b) {
-            System.out.print(b + " ");
-            System.out.println((a-b)/2);
-        }
-        else if(a<b) {
-            System.out.print(a + " ");
-            System.out.println((b-a)/2);
-        }
-        else {
-            System.out.println(a + " ");
-            System.out.println("0");
-        }
+        System.out.println(Math.min(a,b) + " " + (Math.max(a,b) - Math.min(a,b) )/2);
     }
 }
