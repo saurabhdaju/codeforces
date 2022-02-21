c75d5ed914708e7c2baef0971c1ebc3ee22e59ae
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-02-21 11:18:21 +0530

    Updated with better approach

--- P1343B.java
+++ P1343B.java
@@ -8,23 +8,9 @@ public class P1343B {
             int n = sc.nextInt();
             if(n % 4 == 0) {
                 System.out.println("YES");
-                int a =2;
-                int sum = 0;
-                for(int j=1; j<= n/2; j++) {
-                    System.out.print(a + " ");
-                    sum += a;
-                    a +=2;
-                }
-                a =  1;
-                for(int j=1; j<= n/2; j++) {
-                    if(j != n/2) {
-                        System.out.print(a + " ");
-                        sum -= a;
-                        a += 2;
-                    }
-                    else
-                        System.out.println(sum);
-                }
+                for(int i=2; i<=n/2; i +=2) System.out.print((n-i) + " " + (n+i) + " ");
+                for(int i=1; i<n/2; i +=2) System.out.print((n-i) + " " + (n+i) + " ");
+                System.out.println();
             }
             else
                 System.out.println("NO");
