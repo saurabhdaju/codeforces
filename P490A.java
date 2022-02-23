1fa3c86ea0a2714e7bf5c635544d460b81c65f76
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-02-23 11:00:03 +0530

    Removed unwanted lines

--- P490A.java
+++ P490A.java
@@ -7,13 +7,10 @@ public class P490A {
         int[] arr = new int[n+1];
         for(int i=1; i<=n; i++) arr[i] = sc.nextInt();
         int N1s = 0, N2s = 0, N3s = 0;
-        String str1 = "", str2 = "", str3 = "";
         for(int i=1; i<=n; i++) {
             if ((arr[i] == 1)) N1s++;
-            else {
-                if (arr[i] == 2) N2s++;
-                else N3s++;
-            }
+            else if (arr[i] == 2) N2s++;
+            else N3s++;
         }
         int min = Math.min(Math.min(N1s,N2s),N3s);
         System.out.println(min);
