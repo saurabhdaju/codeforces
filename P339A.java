db86052935b6af4c43da1d425d48d1716b6b9b90
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-03-29 10:54:59 +0530

    Solved

--- P339A.java
+++ P339A.java
@@ -1,23 +1,70 @@
 package com.codeforces;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class P339A {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder str = new StringBuilder(br.readLine());
-        int arrSize = (str.length()/2) + 1;
-        int[] arr = new int[arrSize];
-        for(int i=0; i<str.length(); i +=2) {
-            int j=0;
-            arr[j] = str.charAt(i) - '0';
-            System.out.print(arr[j]);
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        String str = sc.next();
+        char[] arr = new char[str.length()/2 + 1];
+        int idx = 0;
+        for(int i=0; i<str.length(); i+=2) {
+            arr[idx] = str.charAt(i);
+            idx++;
         }
         Arrays.sort(arr);
-        String output = "";
-        
+        for(int i=0; i<arr.length/2+1; i++) {
+            System.out.println(i<arr.length-1 ? arr[i] + "+" : arr[i]);
+        }
     }
 }
