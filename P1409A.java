68beb84f402006ce6b4875912636b801652d4333
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-03-29 12:17:56 +0530

    now with better apporach

--- P1409A.java
+++ P1409A.java
@@ -4,23 +4,9 @@ public class P1409A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for (int i=1; i<=t; i++) {
-            int a =sc.nextInt();
-            int b =sc.nextInt();
-            if(a == b)
-                System.out.println("0");
-            else if(a > b) {
-                if((a-b)%10 == 0)
-                    System.out.println((a-b)/10);
-                else
-                    System.out.println(((a-b)/10) + 1);
-            }
-            else {
-                if ((b - a) % 10 == 0)
-                    System.out.println((b-a)/ 10);
-                else
-                    System.out.println(((b-a)/ 10) + 1);
-            }
+        while(t-->0) {
+            long a = sc.nextInt(), b = sc.nextInt(), bin = Math.max(a, b)-Math.min(a, b);
+            System.out.println(bin%10==0 ? bin/10 : bin/10 + 1);
         }
     }
 }
