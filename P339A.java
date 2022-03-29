7f785c9a6c899bdf1572d701c8eae81afebdb6c7
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-03-29 11:18:52 +0530

    better approach

--- P339A.java
+++ P339A.java
@@ -1,70 +1,11 @@
 package com.codeforces;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
 public class P339A {
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader()
-        {
-            br = new BufferedReader(
-                    new InputStreamReader(System.in));
-        }
-
-        String next()
-        {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() { return Integer.parseInt(next()); }
-
-        long nextLong() { return Long.parseLong(next()); }
-
-        double nextDouble()
-        {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine()
-        {
-            String str = "";
-            try {
-                str = br.readLine();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
     public static void main(String[] args) {
-        FastReader sc = new FastReader();
-        String str = sc.next();
-        char[] arr = new char[str.length()/2 + 1];
-        int idx = 0;
-        for(int i=0; i<str.length(); i+=2) {
-            arr[idx] = str.charAt(i);
-            idx++;
-        }
-        Arrays.sort(arr);
-        for(int i=0; i<arr.length/2+1; i++) {
-            System.out.println(i<arr.length-1 ? arr[i] + "+" : arr[i]);
-        }
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.next().split("\\+");
+        Arrays.sort(str);
+        System.out.println(String.join("+",str));
     }
 }
