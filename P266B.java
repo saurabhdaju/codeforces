34577846b972859553bd386328f25516b360d9ae
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-04-04 16:05:17 +0530

    Updated with better approach

--- P266B.java
+++ P266B.java
@@ -4,15 +4,9 @@ public class P266B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), t =sc.nextInt();
-        StringBuilder str = new StringBuilder(sc.next());
+        String str = sc.next();
         while(t-->0) {
-            for(int i=1; i<str.length(); i++) {
-                if(str.charAt(i) == 'G' && str.charAt(i-1) == 'B') {
-                    str.setCharAt(i, 'B');
-                    str.setCharAt(i-1, 'G');
-                    i++;
-                }
-            }
+            str = str.replaceAll("BG", "GB");
         }
         System.out.println(str);
     }
